fb51362b30219d6f9573d0033bc31c2a190a03f1
André Felipe de Barros A. N <100530782+andrefelipebarros@users.noreply.github.com>
2024-06-18 14:05:50 -0300

    Update AutomatizarFaturaRelatorio.py

--- AutomatizarFaturaRelatorio.py
+++ AutomatizarFaturaRelatorio.py
@@ -4,6 +4,7 @@ import time
 def generate_report():
     print("OLÁ, Veja como é funcional!")
 
+#Coloquei para a cada 1 segundo rodar esse script(No caso a função "generate_report()").
 schedule.every(1).second.do(generate_report)
 
 while 1:
